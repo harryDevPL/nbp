9604a41de6eded330ada20f399f313245fb09305
harryDevPL <kristricity@gmail.com>
2022-09-18 20:06:28 +0200

    Initial commit

---
+++ src/main/java/com/sygnity/npb/NpbApplication.java
@@ -0,0 +1,13 @@
+package com.sygnity.npb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpbApplication.class, args);
+	}
+
+}
